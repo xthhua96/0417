005ee004e45d2fc552b4d3df4c3d2b7a6fc6133b
xutianhua <xutianhua@58.com>
2025-04-26 19:54:51 +0800

    0426

--- token_dataset.py
+++ token_dataset.py
@@ -1,18 +1,25 @@
 import re
 import os
 import json
+import threading
+import queue as Queue
 from enum import Enum
 from typing import List
 from typing import Tuple
 from typing import Iterator
 from typing import Any
-from multiprocessing import Pool
 from tqdm import tqdm
 
+import h5py
 import numpy as np
 import torch
 from torch.utils.data import Dataset
+from torch.utils.data import DataLoader
 from modelscope import AutoTokenizer
+
+from stream_dataset import BackgroundGenerator
+from tool import load_config
+from tool import get_paths
 
 system_name = os.uname().sysname
 print(system_name)
@@ -156,11 +163,112 @@ class StreamTokenDataset(Dataset):
                     current_idx += 1
 
 
+class TokenDataset(Dataset):
+    def __init__(
+        self,
+        local_rank: int = 0,
+        window_size: int = 1024,
+        vocab_peak: int = (256, 265),
+    ):
+        super(TokenDataset, self).__init__()
+        config = load_config()
+        _, dataset_path, _ = get_paths(config)
+        self.local_rank = local_rank
+        self.window_size = window_size
+        self.vocab_peak = vocab_peak
+        self.dataset_file = h5py.File(dataset_path, "r")
+        self.labels = self.dataset_file["labels"]
+        self.num = self.dataset_file["number"]
+
+    def __len__(self) -> int:
+        return self.num
+
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        start_idx = (idx // self.window_size) * self.window_size
+        if idx == 0:
+            return self._preprocess_token_ids([-1]), torch.tensor(
+                self.labels[idx], dtype=torch.long
+            )
+        return self._preprocess_token_ids(self.labels[start_idx:idx]), torch.tensor(
+            self.labels[idx], dtype=torch.long
+        )
+
+    def _preprocess_token_ids(self, token_ids: List[int]) -> torch.Tensor:
+        tensor = torch.full(
+            (self.vocab_peak[0] * self.vocab_peak[1],), fill_value=0, dtype=torch.float
+        )
+        valid_len = min(len(token_ids), self.vocab_peak[0] * self.vocab_peak[1])
+        tensor[:valid_len] = torch.tensor(token_ids[-valid_len:], dtype=torch.float)
+        return (
+            tensor.view(self.vocab_peak[0], self.vocab_peak[1])
+            .unsqueeze(0)
+            .repeat(3, 1, 1)
+        )
+
+
+class DataLoaderX(DataLoader):
+    def __init__(self, local_rank, **kwargs):
+        super(DataLoaderX, self).__init__(**kwargs)
+        self.stream = torch.cuda.Stream(local_rank)
+        self.local_rank = local_rank
+
+    def __iter__(self):
+        self.iter = super(DataLoaderX, self).__iter__()
+        self.iter = BackgroundGenerator(self.iter, self.local_rank)
+        self._preload()
+        return self
+
+    def _preload(self):
+        self.batch = next(self.iter, None)
+        if self.batch is None:
+            self.iter = None
+            return
+        with torch.cuda.stream(self.stream):
+            for k in range(len(self.batch)):
+                self.batch[k] = self.batch[k].to(self.local_rank, non_blocking=True)
+
+    def __next__(self):
+        torch.cuda.current_stream().wait_stream(self.stream)
+        batch = self.batch
+        if self.batch is None:
+            raise StopIteration
+        self._preload()
+        return batch
+
+
+class BackgroundGenerator(threading.Thread):
+    def __init__(self, generator, local_rank, max_prefetch=6):
+        super(BackgroundGenerator, self).__init__()
+        self.queue = Queue.Queue(max_prefetch)
+        self.generator = generator
+        self.local_rank = local_rank
+        self.daemon = True
+        self.start()
+
+    def run(self):
+        torch.cuda.set_device(self.local_rank)
+        for item in self.generator:
+            self.queue.put(item)
+        self.queue.put(None)
+
+    def next(self):
+        next_item = self.queue.get()
+        if next_item is None:
+            raise StopIteration
+        return next_item
+
+    def __next__(self):
+        return self.next()
+
+    def __iter__(self):
+        return self
+
+
 if __name__ == "__main__":
     tokenizer = TokenizerType.QWEN_QWQ_32B
     vocab_peak = 256 * 256
     data_type = DatasetType.PRETRAIN_HQ
-    token_dataset = StreamTokenDataset(tokenizer, vocab_peak, data_type)
-    count_num = token_dataset._local_count_samples()
-    print(f"样本数: {count_num}")
+    token_dataset = TokenDataset()
+    print(token_dataset[0][0].shape)
+    # print(f"样本数: {count_num}")
     # print(token_dataset[0][0].shape)
--- tool.py
+++ tool.py
@@ -3,24 +3,81 @@ import yaml
 import json
 import h5py
 
+from tqdm import tqdm
+from modelscope import AutoTokenizer
 
-def process_dump_h5py():
+
+def load_config() -> dict:
     with open("./config.yaml", "r") as f:
-        config = yaml.safe_load(f)
-        sys_name = os.uname().sysname
-        data_dir = ""
-        if sys_name == "Darwin":
-            data_dir = config["dataset"]["DARWIN_DATA_DIR"]
-        elif sys_name == "Linux":
-            data_dir = config["dataset"]["LINUX_DATA_DIR"]
-        else:
-            raise ValueError("Unsupported operating system")
-        file_name = config["dataset"]["file_name"]
-        file_path = os.path.join(data_dir, file_name)
+        return yaml.safe_load(f)
 
-    with open(file_path, "r", encoding="utf-8") as dataset_file:
-        for line in dataset_file:
-            sample = json.loads(line.strip())
-            token_ids = tokenizer.encode(sample["text"], add_special_tokens=True)
 
-    pass
+def get_tokenizer(name: str):
+    return AutoTokenizer.from_pretrained(name)
+
+
+def get_paths(config: dict) -> tuple[str, str, str]:
+    sys_name = os.uname().sysname
+    if sys_name == "Darwin":
+        raw_dir = config["raw_data"]["darwin_data_dir"]
+        dataset_dir = config["dataset"]["darwin_data_dir"]
+    elif sys_name == "Linux":
+        raw_dir = config["raw_data"]["linux_data_dir"]
+        dataset_dir = config["dataset"]["linux_data_dir"]
+    else:
+        raise ValueError(f"Unsupported OS: {sys_name}")
+    return (
+        os.path.join(raw_dir, config["raw_data"]["file_name"]),
+        os.path.join(dataset_dir, config["dataset"]["file_name"]),
+        config["raw_data"]["tokenizer"],
+    )
+
+
+def init_dataset():
+    config = load_config()
+    raw_path, dataset_path, tokenizer_name = get_paths(config)
+    tokenizer = get_tokenizer(tokenizer_name)
+
+    # 配置缓存批量写入参数
+    BATCH_SIZE = 1000
+    buffer_labels = []
+
+    # 创建 HDF5 文件和动态数据集
+    with h5py.File(dataset_path, "w") as h5file:
+        label_ds = h5file.create_dataset(
+            "labels", shape=(0,), maxshape=(None,), dtype="i4"
+        )
+
+        current_size = 0
+        number = 0
+        with open(raw_path, "r", encoding="utf-8") as dataset_file:
+            for line in tqdm(dataset_file, desc="Processing"):
+                sample = json.loads(line.strip())
+                sample_token_ids = tokenizer.encode(
+                    sample["text"], add_special_tokens=True
+                )
+                number += len(sample_token_ids) - 1
+
+                for i in range(len(sample_token_ids)):
+                    buffer_labels.append(sample_token_ids[i])
+
+                    if len(buffer_labels) == BATCH_SIZE:
+                        # 写入当前批次
+                        new_size = current_size + BATCH_SIZE
+                        label_ds.resize((new_size,))
+                        label_ds[current_size:new_size] = buffer_labels
+                        current_size = new_size
+                        buffer_labels.clear()
+
+            # 写入剩余未满一批的数据
+            if buffer_labels:
+                batch_len = len(buffer_labels)
+                new_size = current_size + batch_len
+                label_ds.resize((new_size,))
+                label_ds[current_size:new_size] = buffer_labels
+
+        h5file.create_dataset("number", data=number)
+
+
+if __name__ == "__main__":
+    init_dataset()
